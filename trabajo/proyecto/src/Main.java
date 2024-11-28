91b00b4654234c8fe6a40d3bdb426dfa33e73a84
Hollman <hollmanesnheidert.luna@ikasle.egibide.org>
2024-11-28 21:17:37 +0100

    hg

--- trabajo/proyecto/src/Main.java
+++ trabajo/proyecto/src/Main.java
@@ -2,9 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("Hola mundo");
-        }
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        System.out.println("Hola mundo");
     }
+}
 }
